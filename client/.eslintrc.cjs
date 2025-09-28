9e8e2604a98dc13d000b6b94e4b54911c10ff3b7
Paris Theofanidis <ptheofan@gmail.com>
2025-09-28 10:38:21 +0300

    publisher-service

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -14,5 +14,7 @@ module.exports = {
       'warn',
       { allowConstantExport: true },
     ],
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-unused-vars': 'off', // Turn off base rule as it can report incorrect errors
   },
 }
